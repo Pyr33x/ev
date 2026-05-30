0eb1991fca74c22271b096842adb6ddacb3ef31d
pyr33x <pyrexrj@gmail.com>
2026-05-30 13:28:58 +0330

    feat: attach http server to the main cmd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"ev/internal/server/http"
+	"ev/pkg/config"
+	"log"
+	"os/signal"
+	"syscall"
+	"time"
+
+	h "net/http"
+)
+
+func main() {
+	config := config.New()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	srv := http.New(&config.Server)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != h.ErrServerClosed {
+			log.Println("failed to listen and serve", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("shutting down gracefully...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Println("http server shutdown error", err)
+	}
+
+	log.Println("server stopped gracefully.")
+}
