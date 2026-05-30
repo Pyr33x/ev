48c494e00015e50b55f06689100bb1c271148ea7
pyr33x <pyrexrj@gmail.com>
2026-05-30 14:45:07 +0330

    feat: create kafka adapter (reader/writer)

---
+++ internal/adapter/kafka/kafka.go
@@ -0,0 +1,67 @@
+package kafka
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pyr33x/ev/pkg/config"
+	"github.com/segmentio/kafka-go"
+)
+
+type Adapter struct {
+	writer *kafka.Writer
+	reader *kafka.Reader
+}
+
+func New(ctx context.Context, cfg *config.Kafka) (*Adapter, error) {
+	writer := &kafka.Writer{
+		Addr:     kafka.TCP(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)),
+		Topic:    cfg.Topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	readerConfig := kafka.ReaderConfig{
+		Brokers: []string{fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)},
+		Topic:   cfg.Topic,
+		// Partition: cfg.Partition
+		GroupID:  cfg.GroupID,
+		MinBytes: 1,
+		MaxBytes: 10e6,
+	}
+	reader := kafka.NewReader(readerConfig)
+
+	return &Adapter{
+		writer: writer,
+		reader: reader,
+	}, nil
+}
+
+func (a *Adapter) WriteMessage(ctx context.Context, key, value []byte, headers ...kafka.Header) error {
+	return a.writer.WriteMessages(ctx, kafka.Message{
+		Key:     key,
+		Value:   value,
+		Headers: headers,
+	})
+}
+
+func (a *Adapter) ReadMessage(ctx context.Context) (kafka.Message, error) {
+	return a.reader.ReadMessage(ctx)
+}
+
+func (a *Adapter) Close() error {
+	var writerErr, readerErr error
+
+	if a.writer != nil {
+		writerErr = a.writer.Close()
+	}
+
+	if a.reader != nil {
+		readerErr = a.reader.Close()
+	}
+
+	if writerErr != nil {
+		return writerErr
+	}
+
+	return readerErr
+}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,19 +1,43 @@
 package config
 
-import "github.com/pyr33x/envy"
+import (
+	"github.com/pyr33x/envy"
+)
 
 type Config struct {
 	Server Server
+	Kafka  Kafka
 }
 
 type Server struct {
 	Port int
 }
 
+type Conn struct {
+	Host string
+	Port int
+}
+
+type Kafka struct {
+	Conn
+	GroupID   string
+	Topic     string
+	Partition int
+}
+
 func New() *Config {
 	return &Config{
 		Server: Server{
 			Port: envy.GetInt("PORT", 80),
+		},
+		Kafka: Kafka{
+			Conn: Conn{
+				Host: envy.GetString("KAFKA_HOST", "localhost"),
+				Port: envy.GetInt("KAFKA_PORT", 9092),
+			},
+			GroupID:   envy.GetString("KAFKA_GROUP_ID", "ev_getters"),
+			Topic:     envy.GetString("KAFKA_TOPIC", "pipe"),
+			Partition: envy.GetInt("KAFKA_PARTITION", 0),
 		},
 	}
 }
